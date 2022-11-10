0b2a5e08118c33b2bbc156165222726692f4533f
antony <antony@nadi.ae>
2022-11-10 08:42:14 +0400

    Initial commit

---
+++ Castled/Castled.h
@@ -0,0 +1,18 @@
+//
+//  Castled.h
+//  Castled
+//
+//  Created by Antony Joe Mathew on 10/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Castled.
+FOUNDATION_EXPORT double CastledVersionNumber;
+
+//! Project version string for Castled.
+FOUNDATION_EXPORT const unsigned char CastledVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Castled/PublicHeader.h>
+
+
---
+++ Pods/Target Support Files/Pods-Castled/Pods-Castled-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Castled : NSObject
+@end
+@implementation PodsDummy_Pods_Castled
+@end
